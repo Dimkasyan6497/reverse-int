e83a402e721ea1221a974367c9d20af005b5cd44
Dimkasyan6497 <dmitriiz.97@mail.ru>
2022-01-05 04:53:20 +0400

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {n = -n};
+  n += '';
+  let i = n.length - 1;
+  let result = '';
+  while (i >= 0) {
+    result += n[i];
+    i--;
+  }
+  return result*1;
 }
